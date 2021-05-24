66b9ef69e204abcf8073806b67d28fd9d9f8e011
SM-it-now <ibb09@naver.com>
2021-05-24 18:11:15 +0900

    DRF

---
+++ Django_actual_06/fc_django/fcuser/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import FormView
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth.hashers import check_password, make_password
+from .models import Fcuser
+
+# Create your views here.
+# root 경로 설정
+def index(request):
+    return render(request, 'index.html', { 'email': request.session.get('user') })
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegisterForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        fcuser = Fcuser(
+            email=form.data.get('email'),
+            password=make_password(form.data.get('password')),
+            level='user'
+        )
+        fcuser.save()
+
+        return super().form_valid(form)
+
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = LoginForm
+    success_url = '/'
+
+    # form 유효성 검사가 끝났을 때, 다음 실행되는 부분.
+    def form_valid(self, form):
+        self.request.session['user'] = form.data.get('email')
+        return super().form_valid(form)
+
+def logout(request):
+    if 'user' in request.session:
+        del (request.session['user'])
+
+    return redirect('/')
